5d03d5cfdaec3df07a26a75f8d31751557c7080c
kraksyx <blahblahusdme@gmail.com>
2025-05-26 23:41:38 +0400

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,83 @@
+
+const questions = [
+  {
+    "question": "Which of the following is an advantage of MLS?",
+    "options": [
+      "Can be used in inhospitable terrain",
+      "Uses the same aircraft equipment as ILS",
+      "Has a selective access ability",
+      "Is not affected by heavy precipitation"
+    ],
+    "correct": 0
+  },
+  {
+    "question": "Which answer correctly completes the following statement? The characteristics of an MLS installation are that it uses:",
+    "options": [
+      "an azimuth transmitter at the approach end of the runway, an elevation transmitter at the upwind end of the runway and two frequencies",
+      "one transmitter for both elevation and azimuth and two frequencies",
+      "one transmitter for both elevation and azimuth and a single frequency",
+      "an elevation transmitter at the approach end of the runway, an azimuth transmitter at the upwind end of the runway and a single frequency"
+    ],
+    "correct": 3
+  },
+  {
+    "question": "In a MLS the time that elapses between the passage of the TO scan and the FROM scan at the aircraft position is:",
+    "options": [
+      "not related to the angular position of the aircraft",
+      "indirectly proportional to the angular position of the aircraft",
+      "directly proportional to the angular position of the aircraft",
+      "none of the above are correct"
+    ],
+    "correct": 2
+  },
+  {
+    "question": "A microwave landing system operates:",
+    "options": [
+      "on one of 200 channels in the band 5030 to 5090 GHz",
+      "on one of 200 channels in the band 5.03 GHz to 5090 GHz",
+      "on one of 400 channels in the band 5030 GHz to 5090 GHz",
+      "on one of 400 channels in the band 5.03 GHz to 5.09 GHz"
+    ],
+    "correct": 1
+  },
+  {
+    "question": "MLS consists of:",
+    "options": [
+      "a common azimuth and elevation transmitter",
+      "overlapping centre-line lobe transmission in azimuth angled to provide glide slope information",
+      "an azimuth transmitter and an elevation transmitter on separate frequencies and a DME",
+      "an azimuth transmitter and an elevation transmitter operating on a shared frequency and a DME"
+    ],
+    "correct": 3
+  },
+  {
+    "question": "Which of the following statements is TRUE in respect to microwave landing system?",
+    "options": [
+      "Azimuth and elevation signals use the same aerial on a time share basic",
+      "Azimuth and elevation signals are transmitted at the same UHF frequency",
+      "A special precision DME, operating in the SHF band, provides range information",
+      "Range information is provided by precision DME operating in the UHF"
+    ],
+    "correct": 3
+  },
+  {
+    "question": "In which frequency band does the Microwave Landing System (MLS) operate?",
+    "options": [
+      "“SHF”",
+      "",
+      "",
+      ""
+    ],
+    "correct": 0
+  },
+  {
+    "question": "Distance on MLS is measured by:",
+    "options": [
+      "measuring the time taken for the primary radar pulse to travel from the MLS transmitter to the aircraft receiver",
+      "measuring the time taken for the secondary radar pulse to travel from the MLS transmitter to the aircraft receiver",
+      "phase comparison between the azimuth and elevation beams",
+      "co-located DME"
+    ],
+    "correct": 3
+  }
+];
